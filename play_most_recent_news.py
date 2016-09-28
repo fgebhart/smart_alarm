2defec45e806a2f5114f806dd10ccb963a4cf97a
fgebhart <fabian.gebhart@gmail.com>
2016-09-28 15:09:23 +0200

    new main file (play_most_recent_news.py) + pyttsx

--- play_most_recent_news.py
+++ play_most_recent_news.py
@@ -8,12 +8,18 @@ __author__ = 'fgebhart'
 5. Using pygame lib the news mp3 file will be played
 """
 
+# import packages
+# minidom = xml parser
+# urllib manages to download web files
+# pygame = plays mp3 files
+# python text to speech library
 from xml.dom import minidom
 import urllib2
 import pygame
+import pyttsx
 
 
-# dlf podcast link to XML file. Correct if modified
+# dlf podcast link to XML file. Correct if modified!
 dlf_news_url = "http://www.deutschlandfunk.de/podcast-nachrichten.1257.de.podcast.xml"
 
 
@@ -34,7 +40,7 @@ def download_file(link_to_file):
 
         file_size_dl += len(buffer)
         f.write(buffer)
-
+	
     f.close()
     # XML file now is saved (to the same directory like this file)
     print "download done"
@@ -55,23 +61,40 @@ def find_most_recent_news_url_in_xml_file(xml_file):
 
 
 def play_mp3_file(mp3_file):
+    """using pygame lib in order to play mp3 sound files"""
     pygame.mixer.init()
     pygame.mixer.music.load(mp3_file)
+    print "now playing file: ", mp3_file
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
         continue
 
 
+def say(text):
+    """synthesizes the given text to speech"""
+    engine = pyttsx.init()
+    engine.setProperty('rate', 150)
+    # remove "pass" and uncomment next line in order to enable this function
+    pass    
+    #engine.say(text)
+    engine.runAndWait()
+
+
+
 # download dlf_xml_file according to the dlf_news_url
 dlf_xml_file = download_file(dlf_news_url)
+say("xml file downloaded")
 
 # now parse the dlf_xml_file in order to find the most_recent_news_url
 most_recent_news_url = find_most_recent_news_url_in_xml_file(dlf_xml_file)
+say("url to newest file detected")
 
 # download the most recent news_mp3_file according to the most_recent_news_url
 news_mp3_file = download_file(most_recent_news_url)
+say("mp3 file of news downloaded")
 
 # play the most recent news_mp3_file
+say("now playing the news")
 play_mp3_file(news_mp3_file)
 
 
