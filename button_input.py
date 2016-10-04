4a73f81575ae7d5723b450606931694b83784bd5
Fabian Gebhart <fabian.gebhart@gmail.com>
2016-10-04 23:36:12 +0200

    python code to read out button pressed for RPI

---
+++ button_input.py
@@ -0,0 +1,10 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    print "button is: ", GPIO.input(11)
+    time.sleep(0.4)
