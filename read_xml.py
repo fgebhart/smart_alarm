a59908927dcade76a71e6770273dd46c17fd81c6
Fabian Gebhart <fabian.gebhart@gmail.com>
2016-10-14 17:30:31 +0200

    forgot to keep my old function, since it is relevant, too

--- read_xml.py
+++ read_xml.py
@@ -1,5 +1,20 @@
 from xml.dom import minidom
 from collections import namedtuple
+from xml.dom.minidom import Node
+
+
+def read_xml_file_list(xml_file):
+    """reads the xml file, parse it and save the fetched data to the list
+    called 'xml_data'."""
+    xmldoc = minidom.parse(xml_file)
+    xml_data = []
+
+    for elem in xmldoc.getElementsByTagName('data'):
+        for x in elem.childNodes:
+            if x.nodeType == Node.ELEMENT_NODE:
+                xml_data.append(str(x.childNodes[0].data))
+
+    return xml_data
 
 
 def read_xml_file_namedtuple(xml_file):
@@ -14,8 +29,6 @@ def read_xml_file_namedtuple(xml_file):
     settings.individual_message = xmldoc.getElementsByTagName('individual_message')[0].childNodes[0].data
     settings.text = xmldoc.getElementsByTagName('text')[0].childNodes[0].data
     settings.volume = xmldoc.getElementsByTagName('volume')[0].childNodes[0].data
-
-
 
     return settings
 
