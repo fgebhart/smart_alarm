d8200c65b55606e929df9cfdf56ae8ff49c6f282
Fabian Gebhart <fabian.gebhart@gmail.com>
2016-10-14 15:58:00 +0200

    updating xml read function in order to handle the new xml file

--- read_xml.py
+++ read_xml.py
@@ -1,36 +1,27 @@
 from xml.dom import minidom
-from xml.dom.minidom import Node
-
 from collections import namedtuple
 
 
-def read_xml_file_list(xml_file):
-    """reads the xml file, parse it and save the fetched data to the list
-    called 'xml_data'."""
-    xmldoc = minidom.parse(xml_file)
-    xml_data = []
-
-    for elem in xmldoc.getElementsByTagName('data'):
-        for x in elem.childNodes:
-            if x.nodeType == Node.ELEMENT_NODE:
-                xml_data.append(str(x.childNodes[0].data))
-
-    return xml_data
-
 def read_xml_file_namedtuple(xml_file):
-    settings = namedtuple("settings", "lastmodified alarmtime content days")
+    settings = namedtuple("settings", "alarm_active time content days individual_message text volume")
 
     xmldoc = minidom.parse(xml_file)
 
-    settings.lastmodified = xmldoc.getElementsByTagName('lastmodified')[0].childNodes[0].data
-    settings.alarmtime = xmldoc.getElementsByTagName('alarmtime')[0].childNodes[0].data
+    settings.alarm_active = xmldoc.getElementsByTagName('alarm_active')[0].childNodes[0].data
+    settings.time = xmldoc.getElementsByTagName('time')[0].childNodes[0].data
     settings.content = xmldoc.getElementsByTagName('content')[0].childNodes[0].data
     settings.days = xmldoc.getElementsByTagName('days')[0].childNodes[0].data
+    settings.individual_message = xmldoc.getElementsByTagName('individual_message')[0].childNodes[0].data
+    settings.text = xmldoc.getElementsByTagName('text')[0].childNodes[0].data
+    settings.volume = xmldoc.getElementsByTagName('volume')[0].childNodes[0].data
+
+
 
     return settings
 
 
-
 if __name__ == '__main__':
-    settings = read_xml_file_namedtuple('data.xml')
-    print settings.lastmodified +' '+ settings.alarmtime +' '+ settings.content +' '+ settings.days
+    settings = read_xml_file_namedtuple('xml_dummy.xml')
+    print str(settings.alarm_active) + '\n' + str(settings.time) + '\n' + str(settings.content) + '\n' + str(settings.days) + '\n'\
+          + str(settings.individual_message) + '\n' + str(settings.text) + '\n' + str(settings.volume)
+
