daf49cb734067c618e5518b653957fac2d008994
Fabian Gebhart <fabian.gebhart@gmail.com>
2016-12-28 23:26:09 +0100

    new autostart

--- smart_alarm/autostart.sh
+++ smart_alarm/autostart.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
+# set envirmonetal variable for apache path 
+export smart_alarm_path=/home/pi/smart_alarm/smart_alarm
 
 # activate alternative GIPO functions in order to enable zero-audio
 gpio_alt -p 13 -f 0
 gpio_alt -p 18 -f 5
 
-
-cd smart_alarm/smart_alarm/
-
-# run python web-server
-python python_server.py &
-
-
 # run smart_alarm main script
-sudo python smart_alarm0.8.py &
+sudo python $smart_alarm_path/smart_alarm0.8.py &
